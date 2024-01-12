35fd04909396687dfc4b7529a31c54b9215f8aeb
adhirajChauhan <adhirajsingh0105@gmail.com>
2024-01-12 20:46:06 +0530

    type Conversion

--- Main_JAVA_LPU_G2/02_DataType_Variables/01_Intro.java
+++ Main_JAVA_LPU_G2/02_DataType_Variables/01_Intro.java
@@ -15,16 +15,30 @@ class Intro{
     public static void main(String args[]){
         // System.out.println("Hello World");
 
-        int num1 = 5;
-        int num2 = 10;
-        int result = num1 + num2;
+        // int num1 = 5;
+        // int num2 = 10;
+        // int result = num1 + num2;
 
-        float num3 = 10.5f;
-        byte num4 = 22;
-        Boolean val = true;
-        long num5 = 1111111111111L;
-        double num6 = 99999d;
-        System.out.println(num4);
+        // float num3 = 10.5f;
+        // byte num4 = 22;
+        // Boolean val = true;
+        // long num5 = 1111111111111L;
+        // double num6 = 99999d;
+        // System.out.println(num4);
+
+
+        //-------------------Type conversion
+
+        // int num1 = 5;
+        // double num2 = num1; //automatic
+
+        // System.out.println(num2);
+        // System.out.println(num1);
+
+        double num1 = 15.5;
+        int num2 = (int) num1;
+        System.out.println(num2);
+        System.out.println(num1);
     }
 }
 
