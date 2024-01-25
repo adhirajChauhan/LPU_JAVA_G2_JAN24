78a4f7eecc5f6a663540ec1be629b96ddd4bdf3d
adhirajChauhan <adhirajsingh0105@gmail.com>
2024-01-25 20:54:28 +0530

    25012024

---
+++ Main_JAVA_LPU_G2/05_loops/04_break_cont.java
@@ -0,0 +1,13 @@
+class LPU{
+    public static void main(String a[]){
+
+        for(int i = 0; i < 10; i++){
+
+            // if(i == 6) break;
+
+            if(i == 6) continue;
+
+            System.out.println(i);
+        }
+    }
+}
---
+++ Main_JAVA_LPU_G2/06_Arrays/01_intro.java
@@ -0,0 +1,36 @@
+class LPU{
+    public static void main(String args[]){
+
+        // int arr[] = new int[5];
+        
+        // arr[0] = 10;
+        // arr[4] = 100;
+        // System.out.println(arr[0]);
+        // System.out.println(arr[4]);
+        // System.out.println("Length " + arr.length);
+
+        // int arr[] = {1,2,3,4,5,6};
+
+        // for(int num : arr){
+        //     System.out.println(num);
+        // }
+        // for(int i = 0; i < arr.length; i++){
+        //     System.out.println(arr[i]);
+        // }
+
+
+        int arr[][] = {{1,2,3},{4,5,6},{7,8,9}};
+        System.out.println((arr[2][2]));
+
+    }
+}
+
+
+
+/*
+ Create an array containing roll numbers of your 10 friends, all the friends having even roll number will be printed on console and ignore the odd roll numbers.
+
+
+Create an array of 5 floats and claculate their sum and print
+
+ */
